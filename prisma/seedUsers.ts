e2d18d5d2d2529892bc739c4a37e7d672a62a3f2
YuxianDu <yuxiandu5@gmail.com>
2025-09-18 13:38:32 +1000

    schema/ added role to user schema also seeded users with role

--- prisma/seedUsers.ts
+++ prisma/seedUsers.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { Role } from "@prisma/client";
 import { hash } from "@node-rs/argon2";
 
 const prisma = new PrismaClient();
@@ -11,10 +12,11 @@ async function main() {
     where: {
       userName: "121212",
     },
-    update: {},
+    update: {role: Role.ADMIN},
     create: {
       userName: "121212",
       passwordHash: await hash("121212"),
+      role: Role.ADMIN,
       recoverQuestion: "121212",
       answer: "121212",
       isActive: true,
@@ -26,12 +28,29 @@ async function main() {
     where: {
       userName: "131313",
     },
-    update: {},
+    update: {role: Role.MODERATOR},
     create: {
       userName: "131313",
       passwordHash: await hash("131313"),
+      role: Role.MODERATOR,
       recoverQuestion: "131313",
       answer: "131313",
+      isActive: true,
+      lastLoginAt: null, // Never logged in
+    },
+  });
+
+  const user3 = await prisma.user.upsert({
+    where: {
+      userName: "141414",
+    },
+    update: {},
+    create: {
+      userName: "141414",
+      passwordHash: await hash("141414"),
+      role: Role.USER,
+      recoverQuestion: "141414",
+      answer: "141414",
       isActive: true,
       lastLoginAt: null, // Never logged in
     },
