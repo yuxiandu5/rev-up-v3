efa297f323c845d6cacd061b5d65b3aa330a1730
YuxianDu <yuxiandu5@gmail.com>
2025-10-09 19:39:44 +1100

    removed un exist component import

--- src/app/(app)/market-place/layout.tsx
+++ src/app/(app)/market-place/layout.tsx
@@ -1,9 +1,6 @@
-import MarketPlaceSideBar from "./components/MarketPlaceSideBar";
-
 export default function MarketPlaceLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-full w-full">
-      <MarketPlaceSideBar />
       <section className="flex items-center justify-center overflow-y-auto p-4 md:p-6 lg:p-8 w-full">
         {children}
       </section>
